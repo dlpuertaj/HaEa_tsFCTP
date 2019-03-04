31b14c4ef618c8adcb6bb55e384bdfe3299b501b
David Leonardo <dlpuerta@gmail.com>
2019-03-03 20:46:06 -0500

    Generator Class

---
+++ tsFCTP/src/main/java/dlpuertaj/optimization/utils/Generator.java
@@ -0,0 +1,6 @@
+package dlpuertaj.optimization.utils;
+
+public class Generator {
+    //TODO: Methdod that creates a tsFCTP instances generating the number od facilities based on a percentage
+    //TODO: Method that generates fixed costs and random costs based on 2018 paper given the size of the problem
+}
